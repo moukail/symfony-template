9ed846eda54b2d902a1406f631489af55a2820d1
ismail <ismail@logitrade.nl>
2023-10-01 09:29:59 +0200

    update

--- .docker/php/docker-init.sh
+++ .docker/php/docker-init.sh
@@ -1,58 +1,31 @@
 #!/usr/bin/env bash
 
-# create new project
+# create new project if not exists
 if [ ! -d "./public" ]; then
   . /home/install.sh
 fi
 
-#rm -rf node_modules package-lock.json
-#npm install --silent --no-progress --non-interactive
-#npm audit fix
-#npm install --global browser-sync
-#npm run dev
-
-#export APP_ENV=dev
-#rm -rf var vendor composer.lock symfony.lock
 #cp .env.local .env
-echo "8.2" > .php-version
 
-#symfony --no-interaction self:update
-#symfony self:version
-#symfony self:cleanup
-
+echo "--------------------------------------------------------------------"
+echo "-                            composer                              -"
+echo "--------------------------------------------------------------------"
 composer remove --unused
 composer validate
-
-echo "-----------------------------------------------------------------------------------------------------------------"
-echo "-                                                composer                                                       -"
-echo "-----------------------------------------------------------------------------------------------------------------"
-symfony composer install --no-interaction #--no-plugins --no-scripts
+symfony composer install --no-interaction
 symfony composer -n check-platform-reqs
 
 echo "-------------------------------------------------------------------"
 echo "-                        waiting for DB                           -"
 echo "-------------------------------------------------------------------"
 while ! nc -z $DATABASE_HOST $DATABASE_PORT; do sleep 1; done
+
 echo "-------------------------------------------------------------------"
 echo "-                        prepare the DB                           -"
 echo "-------------------------------------------------------------------"
-#symfony console doctrine:database:drop --if-exists --force
 symfony console doctrine:database:create --if-not-exists
 symfony console doctrine:migrations:migrate --no-interaction
 symfony console doctrine:fixtures:load --no-interaction -vvv
-
-#symfony console doctrine:database:drop --if-exists --force
-#symfony console doctrine:database:create --if-not-exists
-#symfony console doctrine:migrations:up-to-date
-#symfony console doctrine:migrations:migrate --allow-no-migration --no-interaction
-#symfony console doctrine:schema:update --force
-#symfony console doctrine:schema:validate
-#symfony console doctrine:fixtures:load --no-interaction
-#nohup symfony console thruway:router:start >/dev/null 2>&1 &
-
-#export PHP_IDE_CONFIG="serverName=your-server-name-configured-in-php-storm"
-#export XDEBUG_CONFIG="remote_host=host.docker.internal idekey=PHPSTORM"
-#export XDEBUG_SESSION_START=10219
 
 echo "-------------------------------------------------------------------"
 echo "-                        php-cs-fixer                             -"
@@ -89,28 +62,26 @@ echo "-------------------------------------------------------------------"
 echo "-------------------------------------------------------------------"
 echo "-                        website is ready                         -"
 echo "-------------------------------------------------------------------"
-#symfony proxy:start
-#symfony proxy:domain:attach my-domain
-#HTTPS_PROXY=http://127.0.0.1:7080 curl https://my-domain.wip
+
 chmod -R a+rw ./
 symfony local:php:list
 symfony local:php:refresh
 symfony check:requirements
 symfony check:security
 symfony console about
-#echo | symfony server:ca:install
-#symfony serve --p12=/var/www/certs/localhost.p12
+
 symfony serve
 
 echo "-------------------------------------------------------------------"
 echo "-                        testing                                  -"
 echo "-------------------------------------------------------------------"
-#codecept clean
-#codecept run --steps --debug -vvv --coverage --coverage-xml --coverage-html
+codecept clean
+codecept run --steps --debug -vvv --coverage --coverage-xml --coverage-html
 
 echo "-------------------------------------------------------------------"
 echo "-                        yarn watch                               -"
 echo "-------------------------------------------------------------------"
-#symfony run -d npm run watch
+npm install --non-interactive
+yarn watch
 
 tail -f /dev/null
--- README.md
+++ README.md
@@ -35,3 +35,73 @@ The error output from the command was:
 
 /sbin/mount.vboxsf: mounting failed with the error: No such device
 ```
+
+https://theboroer.github.io/localtunnel-www/
+
+### install ngrok
+https://ngrok.com/docs/getting-started/
+https://dashboard.ngrok.com/get-started/setup
+```bash
+sudo snap install ngrok
+ngrok config add-authtoken <YOUR_TOKEN>
+ngrok http --domain=noted-shad-flexible.ngrok-free.app 8000
+ngrok tunnel --label edge=edghts_2W8TyJQmPfk5xw5pp6mxcPcENN5 http://localhost:8000
+```
+
+### Dockerfile
+```bash
+# Xdebug =<3.1 support PHP 7.4
+#RUN pecl install xdebug-3.1.6 && docker-php-ext-enable xdebug
+```
+
+```bash
+symfony/amqp-pack ramsey/uuid-doctrine symfony/mailgun-mailer symfony/mercure-bundle
+symfony composer require --no-interaction -W sensio/framework-extra-bundle
+symfony composer require --no-interaction nelmio/cors-bundle gesdinet/jwt-refresh-token-bundle sentry/sentry-symfony
+symfony composer require --no-interaction moukail/mailgun-mail-status-bundle moukail/password-reset-mail-bundle moukail/verification-mail-bundle
+symfony composer require --no-interaction dompdf/dompdf nyholm/psr7
+symfony composer require --no-interaction league/flysystem-aws-s3-v3:2.0.* league/flysystem-bundle #league/flysystem-cached-adapter
+#symfony/messenger
+#twig/extra-bundle
+
+#symfony composer require --dev codeception/codeception codeception/specify codeception/verify ericmartel/codeception-email-mailhog
+#symfony composer require --dev league/factory-muffin league/factory-muffin-faker
+#symfony composer require --dev flow/jsonpath phpbench/phpbench
+#symfony composer require --dev vimeo/psalm
+
+#mkdir -p config/secrets
+#openssl genpkey -out config/secrets/private.pem -aes256 -algorithm rsa -pkeyopt rsa_keygen_bits:4096
+#openssl pkey -in config/secrets/private.pem -out config/secrets/public.pem -pubout
+
+
+#symfony composer config extra.symfony.allow-contrib true
+#symfony composer config allow-plugins.symfony/runtime true
+```
+#echo | symfony server:ca:install
+#symfony serve --p12=/var/www/certs/localhost.p12
+
+#symfony --no-interaction self:update
+#symfony self:version
+#symfony self:cleanup
+
+#symfony console doctrine:database:drop --if-exists --force
+#symfony console doctrine:database:create --if-not-exists
+#symfony console doctrine:migrations:up-to-date
+#symfony console doctrine:migrations:migrate --allow-no-migration --no-interaction
+#symfony console doctrine:schema:update --force
+#symfony console doctrine:schema:validate
+#symfony console doctrine:fixtures:load --no-interaction
+#nohup symfony console thruway:router:start >/dev/null 2>&1 &
+
+#export PHP_IDE_CONFIG="serverName=your-server-name-configured-in-php-storm"
+#export XDEBUG_CONFIG="remote_host=host.docker.internal idekey=PHPSTORM"
+#export XDEBUG_SESSION_START=10219
+
+#symfony proxy:start
+#symfony proxy:domain:attach my-domain
+#HTTPS_PROXY=http://127.0.0.1:7080 curl https://my-domain.wip
+
+#npm audit fix
+#npm install --global browser-sync
+#npm run dev
+#symfony run -d npm run watch
